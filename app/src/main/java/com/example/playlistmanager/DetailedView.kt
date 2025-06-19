701a3ebc354680da6eb7a00853304e34d3551efa
Sibusisekile Sithole <ST10481493@vcconnect.edu.za>
2025-06-19 11:17:41 +0200

    Added the three button with their right functions

--- app/src/main/java/com/example/playlistmanager/DetailedView.kt
+++ app/src/main/java/com/example/playlistmanager/DetailedView.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmanager
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +10,18 @@ import androidx.core.view.WindowInsetsCompat
 
 class DetailedView : AppCompatActivity() {
     private lateinit var textViewSummary: TextView
+    private lateinit var displayBtn: Button
+    private lateinit var returnBtn: Button
+    private lateinit var averageBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detailed_view)
 
         val textView = findViewById<TextView>(R.id.textViewSummary)
+        displayBtn = findViewById<Button>(R.id.displayBtn)
+        returnBtn = findViewById<Button>(R.id.returnBtn)
+        averageBtn = findViewById<Button>(R.id.averageBtn)
 
         val song = intent.getStringArrayListExtra("songs") ?: arrayListOf()
         val artist = intent.getStringArrayListExtra("artists") ?: arrayListOf()
@@ -23,12 +30,16 @@ class DetailedView : AppCompatActivity() {
 
         val averageRating = if (ratings.isNotEmpty()) ratings.sum() / ratings.size else 0
 
+        displayBtn.setOnClickListener {
         val summary = StringBuilder()
         for (i in song.indices) {
             summary.append("Song: $song, Artist: $artist, Ratings: $ratings, Comments: $comments")
         }
-        summary.append("\nAverage Ratings: $averageRating")
 
         textView.text = summary.toString()
     }
-}
+        averageBtn.setOnClickListener {
+    val summary = StringBuilder()
+    summary.append("\nAverage Ratings: $averageRating")
+            textView.text = summary.toString()
+}}}
